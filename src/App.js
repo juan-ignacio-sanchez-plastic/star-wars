ffd344f176a8075ea2783686b1b51eae82ee61b9
Juan Sánchez <juansp92@hotmail.com>
2021-03-08 16:06:39 -0300

    App styles changes to sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import React from 'react';
+import { Home } from './views';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.sass';
 
 function App () {
   return (
     <div className="App">
+      <Router>
+        <Route exact path='/'><Home /></Route>
+      </Router>
     </div>
   );
 }
