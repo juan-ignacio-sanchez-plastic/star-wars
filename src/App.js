e092bf191b9de1155064cfedff95689b25e06643
Juan Sánchez <juansp92@hotmail.com>
2021-03-09 19:35:26 -0300

    host url for netlify

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.scss';
 
+const { HOST } = process.env;
+
 function App () {
   return (
     <div className="App">
       <div className='twinkling'>
         <Router>
           <Route exact path='/'><Home /></Route>
-          <Route path='/character-details/:characterID/:characterName'><CharacterDetails /></Route>
+          <Route path={`${HOST}/character-details/:characterID/:characterName`}><CharacterDetails /></Route>
         </Router>
       </div>
     </div>
