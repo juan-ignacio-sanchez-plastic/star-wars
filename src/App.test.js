a98372e7634ed2ddfbb2f1eabf01ba94d4bca6da
Juan Sánchez <juansp92@hotmail.com>
2021-03-10 16:47:42 -0300

    tests added in App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders and unmount App', () => {
+  const { unmount } = render(<App />);
+  unmount();
 });
+
+window.HTMLMediaElement.prototype.load = () => { };
+window.HTMLMediaElement.prototype.play = () => { };
+window.HTMLMediaElement.prototype.pause = () => { };
+window.HTMLMediaElement.prototype.addTextTrack = () => { };
